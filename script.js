a67a943897bee91ac311baea01a68f655df02106
Sheen <shaaheenk@gmail.com>
2020-07-21 18:41:19 -0500

    Add files via upload
    fixed score screen on game end.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var checkDead = setInterval(function () {
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
         block.style.animation = "none";
-        alert("You lose.");
+        alert("Game over. Your score was: " +Math.floor(counter/100));
         counter = 0;
         block.style.animation = "block 1s infinite linear";
     } else {
